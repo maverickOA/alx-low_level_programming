75b595851e9d050417a17b15d485869527a3f09d
maverickOA <kingmaverick072@gmail.com>
2023-02-16 18:22:36 -0800

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,9 @@
+#include <unistd.h>
+#include <string.h>
+
+int main(void)
+{
+    char str[] = "and that piece of art is useful - Dora Korpar, 2015-10-19\n";
+    write(2, str, strlen(str));
+    return 1;
+}
