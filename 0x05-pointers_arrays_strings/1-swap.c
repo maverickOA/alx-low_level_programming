1401e0a0eeecb26f558be6ee877f4fe848e82b06
maverickOA <kingmaverick072@gmail.com>
2023-02-27 01:12:11 -0800

    task 2

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -9,8 +9,7 @@
  */
 
 void swap_int(int *a, int *b)
-/* the function that swaps the values of two integers. 
-*/
+/* the function that swaps the values of two integers.*/
 {
 	int temp = *a;
 	*a = *b;
