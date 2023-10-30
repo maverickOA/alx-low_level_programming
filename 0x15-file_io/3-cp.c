fe5d62f962f88a8dae24a4e6169c54e916c0fe12
maverickOA <your-email@example.com>
2023-10-30 14:22:29 -0700

    correction

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,8 +1,18 @@
 #include "main.h"
 
+/**
+ * main - Copy contents from a file to another.
+ *
+ * @argc: The number of arguments specified
+ * @argv: The arguments given
+ *
+ * Return: 0 if the operation succeeds otherwise
+ * -1
+ *
+ **/
 int main(int argc, char *argv[])
 {
-	int file_descriptor, chars_count;
+	int file_from, file_to, chars_read, chars_written;
 	char *str = malloc(sizeof(char) * 1024);
 
 	if (argc != 3)
@@ -12,8 +22,8 @@ int main(int argc, char *argv[])
 	}
 	if (str == NULL)
 		return (-1);
-	file_descriptor = open(argv[1], O_RDONLY);
-	if (file_descriptor == -1)
+	file_from = open(argv[1], O_RDONLY);
+	if (file_from == -1)
 	{
 		from_error(argv[1]);
 	}
@@ -38,21 +48,45 @@ int main(int argc, char *argv[])
 	return (0);
 }
 
+/**
+ * from_error - Print an error message to
+ * standard error with exit code 98 when source
+ * file read operation fails or file cannot
+ * open.
+ *
+ * @str: The file the operation is performed
+ * on
+ */
 void from_error(char *str)
 {
 	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", str);
 	exit(98);
 }
 
+/**
+ * to_error - Print an error message to
+ * standard error with exit code 99 when file
+ * write operation to destination file fails.
+ *
+ * @str: The file the operation is performed
+ * on
+ */
 void to_error(char *str)
 {
 	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", str);
 	exit(99);
 }
 
+/**
+ * close_error - Print an error message to
+ * standard error with exit code 100 when
+ * the close operation fails.
+ *
+ * @file_descriptor: The file descriptor to the file
+ * operation is performed on
+ */
 void close_error(int file_descriptor)
 {
 	dprintf(STDERR_FILENO, "Error: Can't close fd %d", file_descriptor);
 	exit(100);
 }
-
